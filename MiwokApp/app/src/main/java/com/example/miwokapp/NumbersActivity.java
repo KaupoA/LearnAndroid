e18c75f7494ca46cfec97056967ef2a4278e34a2
Kaupo <kaupo300@hotmail.com>
2019-11-23 17:54:31 +0200

    Miwok App - Udacity Tutorial

--- MiwokApp/app/src/main/java/com/example/miwokapp/NumbersActivity.java
+++ MiwokApp/app/src/main/java/com/example/miwokapp/NumbersActivity.java
@@ -3,6 +3,9 @@ package com.example.miwokapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // Make a new ArrayList named words
+        ArrayList<String> words = new ArrayList<>();
+
+        // Add items to the ArrayList
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        ArrayAdapter<String> wordsAdapter = new ArrayAdapter<>(this, R.layout.list_item, words);
+
+        ListView numbersListView = findViewById(R.id.numbersListView);
+        numbersListView.setAdapter(wordsAdapter);
     }
 }
